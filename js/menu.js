82c18fd6787d65ecf81f3f194390609188519c42
gmtw <julio_somo@ciencias.unam.mx>
2019-12-29 19:29:09 -0600

    Corrección del menú adaptable
    Se corrigió que el menú regresara al header y se les agregó los links

--- js/menu.js
+++ js/menu.js
@@ -4,18 +4,23 @@ var contador = 1;
 
 function main() {
 	$('.menu_bar').click(function(){
-
+		event.preventDefault();
 		if (contador == 1) {
 			$('.menu').animate({
 				left:'0'
-				
+				// position = 'absolute'
+			
 			});
+			
+
 			contador = 0;
 		} else{
 			contador = 1;
 			$('.menu').animate({
 				left:'-100%'
+				// position = 'assign(url: DOMString)bsolute'
 			});
+			
 		}
 	})
 }
