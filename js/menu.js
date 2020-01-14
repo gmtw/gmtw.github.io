816be5b063a791cd9590c31050195d525874f9cc
gmtw <julio_somo@ciencias.unam.mx>
2020-01-14 12:51:26 -0600

    Finalización cuadro de advertencia

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@ $(document).ready(main);
 
 var contador = 1;
 
+
 function main() {
 	$('.menu_bar').click(function(){
 		event.preventDefault();
@@ -20,7 +21,46 @@ function main() {
 				left:'-100%'
 				// position = 'assign(url: DOMString)bsolute'
 			});
-			
+
 		}
-	})
+	});
+
+	// $('.contacto').scroll(function () {
+	// 	// $('.costruccion').animate({
+	// 	// 	left: '25%'
+	// 	// });
+	// 	console.log('hola');
+	// });
+
+	$(window).scroll(function () {
+		event.preventDefault();
+		// if ($(window).scrollTop() > 1415 && $(window).scrollTop() < 1690) {
+		// 	console.log('abierto');
+		// 	$('.construccion').animate({
+		// 	left: '10%'
+		// 	});
+		//  } else {
+		//  	console.log('cerrado');
+		//  	$('.costruccion').animate({
+		//  	left: '100%'
+		//  	});
+		// }
+
+		if ($(window).scrollTop() > 1415 && $(window).scrollTop() < 1690) {
+			$('.construccion').animate({
+			left: '20%'
+			});
+			console.log('abierto')
+		}
+
+		// if ($(window).scrollTop() < 1415 || $(window).scrollTop() > 1690) {
+		// 	$('.construccion').animate({
+		// 	left: '100%'
+		// 	});
+		// 	console.log('cerrado')
+		// }
+
+
+	});
+	
 }
